d58ea0b97dd43d11a156064e51c10f7f9abfff5e
PhilMoran <c00192724@itcarlow.ie>
2017-11-24 10:30:51 +0000

    first commit

---
+++ AI Lab1/AI Lab1/Game.cpp
@@ -0,0 +1,189 @@
+// author Peter Lowe
+
+#include "Game.h"
+#include <math.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+Game::Game() :
+	m_window{ sf::VideoMode::getDesktopMode(), "SpaceStationRescue2" ,sf::Style::Fullscreen },
+	m_exitGame{false} //when true game will exit
+{
+	LoadTextures(); // load font 
+	LoadSprites(); // load texture
+}
+
+
+Game::~Game()
+{
+}
+
+
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	sf::Time timePerFrame = sf::seconds(1.f / 60.f); // 60 fps
+	while (m_window.isOpen())
+	{
+		PlayerMovement();
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+/// <summary>
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+/// </summary>
+void Game::processEvents()
+{
+	sf::Event event;
+	
+	
+
+	while (m_window.pollEvent(event))
+	{
+		if ( sf::Event::Closed == event.type) // window message
+		{
+			m_window.close();
+		}
+		if (sf::Event::KeyPressed == event.type) //user key press
+		{
+			if (sf::Keyboard::Escape == event.key.code)
+			{
+				m_exitGame = true;
+			}
+			if (sf::Keyboard::A == event.key.code)
+			{
+				playerSprite.setRotation(playerSprite.getRotation() - 5);
+			
+			}
+			
+			if (sf::Keyboard::D == event.key.code)
+			{
+				playerSprite.setRotation(playerSprite.getRotation() + 5);
+				
+
+			}
+			if (sf::Keyboard::W == event.key.code)
+			{
+				playerVelocity.x += 0.1;
+				if (playerVelocity.x >= 1.0f)
+				{
+					playerVelocity.x = 1.0f;
+				}
+				
+			}
+
+			if (sf::Keyboard::S == event.key.code)
+			{
+				playerVelocity.x -= 0.1;
+				if (playerVelocity.x <= -1.0f)
+				{
+					playerVelocity.x = -1.0f;
+				}
+			}
+
+		}
+		
+	}
+}
+
+
+void Game::Flee()
+{
+	/*Velocity = my.position - target.position
+	Velocity = normalize(velocity)
+	Velocity = velocity * maxSpeed
+	Orientation = getNewOrientation(orientation, velocity)*/
+}
+void Game::PlayerMovement()
+{
+	if (playerSprite.getPosition().y <-500)
+	{
+		playerSprite.setPosition(playerSprite.getPosition().x, 2200);
+	}
+	if (playerSprite.getPosition().y >3500)
+	{
+		playerSprite.setPosition(playerSprite.getPosition().x, 0);
+	}
+	if (playerSprite.getPosition().x>5000)
+	{
+		playerSprite.setPosition(0, playerSprite.getPosition().y);
+	}
+	if (playerSprite.getPosition().x <-500)
+	{
+		playerSprite.setPosition(4000, playerSprite.getPosition().y);
+	}
+	playerSprite.setOrigin(305, 165);
+
+	playerSprite.move(sin(playerSprite.getRotation()*3.14159265 / 180)*playerVelocity.x, -cos(playerSprite.getRotation()*3.14159265 / 180)*playerVelocity.x);
+
+	
+	std::cout << playerVelocity.x << std::endl;
+	
+
+	//Sprite.Move (0, -100 * ElapsedTime);
+	
+}
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+}
+
+/// <summary>
+/// draw the frame and then switch bufers
+/// </summary>
+void Game::render()
+{
+	m_window.clear(sf::Color::White);
+	m_window.draw(playerSprite);
+	m_window.display();
+}
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::LoadTextures()
+{
+	if (!playerTexture.loadFromFile("ASSETS\\IMAGES\\PLAYER.png"))
+	{
+		// error...
+	}
+}
+
+/// <summary>
+/// load the texture and setup the sprite for the logo
+/// </summary>
+void Game::LoadSprites()
+{
+	
+	playerSprite.setTexture(playerTexture);
+	playerSprite.setPosition(1000, 1300.0f);
+}
+
+sf::Vector2f Game::normalize(sf::Vector2f & source)
+{
+	float length = sqrt((source.x * source.x) + (source.y * source.y));
+	if (length != 0)
+		return sf::Vector2f(source.x / length, source.y / length);
+	else
+		return source;
+}
---
+++ AI Lab1/AI Lab1/Game.h
@@ -0,0 +1,50 @@
+// author Peter Lowe
+#ifndef GAME
+#define GAME
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void update(sf::Time t_deltaTime);
+	void render();
+	void AIMovement();
+	void Seek();
+	void Flee();
+	void PlayerMovement();
+	void LoadTextures();
+	void LoadSprites();
+	sf::Vector2f normalize(sf::Vector2f& source);
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+	sf::Font m_ArialBlackfont; // font used by message
+	sf::Text m_welcomeMessage; // text used for message on screen
+	sf::Texture m_logoTexture; // texture used for sfml logo
+	sf::Sprite m_logoSprite; // sprite used for sfml logo
+	bool m_exitGame; // control exiting game
+	sf::Vector2f playerVelocity;
+	sf::Vector2f seekVelocity;
+	float steering;
+	sf::Vector2f playerVelocityAdditive;
+	sf::Vector2f direction;
+	sf::Clock clock;
+	float accceleration = 0;
+	sf::Time elapsed1;
+
+};
+
+#endif // !GAME
+
